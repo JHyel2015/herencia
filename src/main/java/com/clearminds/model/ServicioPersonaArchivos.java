04b489b94ade4b0849cd8b34af6fe7ee44297e65
Richard Heredia <elgeorh@gmail.com>
2020-12-10 10:31:38 -0500

    Modificando los prints y metodo insertarpersona

--- src/main/java/com/clearminds/model/ServicioPersonaArchivos.java
+++ src/main/java/com/clearminds/model/ServicioPersonaArchivos.java
@@ -3,17 +3,17 @@ package com.clearminds.model;
 public class ServicioPersonaArchivos implements ServicioPersona{
 
 	public boolean insertar(Persona persona) {
-		System.out.println("Insertando persona en la BDD " + persona);
+		System.out.println("Insertando persona en un archvio." + persona);
 		return true;
 	}
 
 	public boolean actualizar(Persona persona) {
-		System.out.println("Actualizando persona en la BDD " + persona);
+		System.out.println("Actualizando persona en un archivo. " + persona);
 		return true;
 	}
 
 	public boolean eliminar(Persona persona) {
-		System.out.println("Eliminando persona en la BDD " + persona);
+		System.out.println("Eliminando persona en un archivo." + persona);
 		return true;
 	}
 
