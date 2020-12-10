b1b15599e6605d2f3b064a4fe3ba814c9f1bed33
Richard Heredia <elgeorh@gmail.com>
2020-12-10 11:02:33 -0500

    Instancia dinamica con config.properties

--- src/main/java/com/clearminds/test/TestManager.java
+++ src/main/java/com/clearminds/test/TestManager.java
@@ -1,5 +1,10 @@
 package com.clearminds.test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.clearminds.excepciones.InstanceException;
 import com.clearminds.model.Persona;
 import com.clearminds.model.PersonaManager;
@@ -7,9 +12,19 @@ import com.clearminds.model.PersonaManager;
 public class TestManager {
 
 	public static void main(String[] args) {
+
 		PersonaManager personaManager;
+		Properties p = new Properties();
 		try {
-			personaManager = new PersonaManager("com.clearminds.impl.ServicioPersonaArchivos");
+			p.load(new FileReader("config.properties"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String propiedad = p.getProperty("propiedad1");
+		try {
+			personaManager = new PersonaManager(propiedad);
 			personaManager.insertarPersona(new Persona("Luis", "Casa", 25));
 		} catch (InstanceException e) {
 			e.printStackTrace();
